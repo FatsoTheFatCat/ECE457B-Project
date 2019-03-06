1e856a33096d6989b798ecf8fbcdf3108560830b
Amy <amy.hu.94@gmail.com>
2019-03-06 11:42:17 -0500

    Create placeholder main file and placeholder traffic light model for shape-based detection

---
+++ TrafficLightModel.py
@@ -0,0 +1,8 @@
+from TrafficLight import TrafficLight
+
+# Model of a common Ontario traffic light
+
+# TO DO: export the model "as a constant"
+trafficLightModel = TrafficLight();
+
+# TO DO: create helper function to scale the model
---
+++ main.py
@@ -0,0 +1,9 @@
+from Image import Image
+
+
+# file handling
+# for Images, the typical library is PIP, and the easy-to-install fork is pillow
+
+
+img = Image(5, 8);
+img.getPixel(3, 2);
